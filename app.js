1040000ce911e4adfc9e11bbdb639eba2dafac1d
bony237 <nbj.ngoube@gmail.com>
2021-08-21 00:23:28 +0100

    newbies deployment pb fixed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const port = process.env.PORT || 3000;
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./routers/contactRouter");
@@ -11,4 +12,4 @@ app.use(express.urlencoded({ extended: true }));
 // app.get('/', (req, res) => )
 app.use(contactRouter);
 
-app.listen(3000, () => console.log("Server running at port 3000"));
+app.listen(port, () => console.log("Server running at port 3000"));
